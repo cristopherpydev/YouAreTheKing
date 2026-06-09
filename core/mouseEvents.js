35dd4680fabfd887a3ec06d0f29333381c536664
R3D <cristophermendezcervantes@gmail.com>
2026-06-09 23:38:03 +0100

    added mouse events and tunes to ux

---
+++ core/mouseEvents.js
@@ -0,0 +1,19 @@
+const leftBtn = document.getElementById('btn-left');
+const rightBtn = document.getElementById('btn-right');
+const leftBtnSel = document.querySelector(".btn-left");
+const rightBtnSel = document.querySelector(".btn-right");
+
+leftBtn.addEventListener("mouseover", glowUpMetrics)
+leftBtn.addEventListener("mouseout", resetGlowUp)
+
+rightBtn.addEventListener("mouseover", glowUpMetrics)
+rightBtn.addEventListener("mouseout", resetGlowUp)
+
+function glowUpMetrics(){
+    /* glows up the metrics to give visual feedback to the users */
+    this.style.animation = "glow 1.5s infinite alternate";
+}
+
+function resetGlowUp(){
+    this.style.animation = "none";
+}
